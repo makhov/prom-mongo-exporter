27361dc910783a15190535f075f6f1cb9be4f484
Aleksei Makhov <aleksei.makhov@unity3d.com>
2019-10-21 13:57:43 +0300

    sig trap

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var (
@@ -16,4 +23,22 @@ func main() {
 		return
 	}
 
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+
+	mux := http.NewServeMux()
+
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	<-sig
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	log.Fatal(s.Shutdown(ctx))
 }
